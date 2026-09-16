77896179c0570ed1b25e92181accef08cff43ff2
erdevendrapsingh-lang <er.devendrapsingh@gmail.com>
2026-09-16 18:24:52 +0530

    Add VPC and EKS modules with ECR repository resource

--- variables.tf
+++ variables.tf
@@ -1,1 +1,78 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.0"
 
+  name = "${var.cluster_name}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = ["ap-south-1a", "ap-south-1b"]
+
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  public_subnets = [
+    "10.0.101.0/24",
+    "10.0.102.0/24"
+  ]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Project   = "aws-eks-github-actions"
+    Terraform = "true"
+  }
+}
+
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = var.cluster_name
+  kubernetes_version = var.kubernetes_version
+
+  endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    devops = {
+      name           = "devops-nodegroup"
+      instance_types = [var.node_instance_type]
+
+      min_size     = 2
+      max_size     = 2
+      desired_size = 2
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "aws-eks-github-actions"
+    Terraform   = "true"
+  }
+}
+
+
+resource "aws_ecr_repository" "app" {
+  name                 = var.ecr_repository_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Project   = "aws-eks-github-actions"
+    Terraform = "true"
+  }
+}
