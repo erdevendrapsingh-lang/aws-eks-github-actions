cf104be4ab2d1d0bc3e2544d968ef0cc3748aa88
erdevendrapsingh-lang <er.devendrapsingh@gmail.com>
2026-09-16 23:40:59 +0530

    Add S3 backend configuration to providers.tf
    Configure S3 backend for Terraform state management.

--- providers.tf
+++ providers.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.5.7"
-
+backend "s3" {
+  bucket       = "devops-terraform-state-182011941346"
+  key          = "aws-eks-github-actions/terraform.tfstate"
+  region       = "ap-south-1"
+  encrypt      = true
+  use_lockfile = true
+}
   required_providers {
     aws = {
       source  = "hashicorp/aws"
