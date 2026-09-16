705467d0fd124538c42a839730d6158c27469019
erdevendrapsingh-lang <er.devendrapsingh@gmail.com>
2026-09-16 18:29:14 +0530

    Add outputs for EKS cluster and VPC details

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,24 @@
+output "eks_cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
 
+output "eks_cluster_endpoint" {
+  description = "EKS cluster API endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_arn" {
+  description = "EKS cluster ARN"
+  value       = module.eks.cluster_arn
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "ecr_repository_url" {
+  description = "ECR repository URL"
+  value       = aws_ecr_repository.app.repository_url
+}
