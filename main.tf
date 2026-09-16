e1b23f53c05e58eeae48a6796f4be328c9c04996
erdevendrapsingh-lang <er.devendrapsingh@gmail.com>
2026-09-16 19:13:22 +0530

    Fix formatting and whitespace in main.tf

--- main.tf
+++ main.tf
@@ -9,16 +9,16 @@ module "vpc" {
 
   private_subnets = [
     "10.0.1.0/24",
-    "10.0.2.0/24"
+    "10.0.2.0/24",
   ]
 
   public_subnets = [
     "10.0.101.0/24",
-    "10.0.102.0/24"
+    "10.0.102.0/24",
   ]
 
   enable_nat_gateway = true
-  single_nat_gateway = true
+  single_nat_gateway  = true
 
   enable_dns_hostnames = true
   enable_dns_support   = true
@@ -73,4 +73,4 @@ resource "aws_ecr_repository" "app" {
     Project   = "aws-eks-github-actions"
     Terraform = "true"
   }
-}  
+} 
