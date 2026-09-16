bb3e7f580c2aebe506bf1e4a052838e26258f409
erdevendrapsingh-lang <er.devendrapsingh@gmail.com>
2026-09-16 18:26:43 +0530

    Refactor VPC module configuration in main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,35 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.0"
+
+  name = "${var.cluster_name}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = ["ap-south-1a", "ap-south-1b"]
+
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  public_subnets = [
+    "10.0.101.0/24",
+    "10.0.102.0/24"
+  ]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Project   = "aws-eks-github-actions"
+    Terraform = "true"
+  }
+}
+
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
@@ -30,36 +62,6 @@ module "eks" {
   }
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 6.0"
-
-  name = "${var.cluster_name}-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs = ["ap-south-1a", "ap-south-1b"]
-
-  private_subnets = [
-    "10.0.1.0/24",
-    "10.0.2.0/24"
-  ]
-
-  public_subnets = [
-    "10.0.101.0/24",
-    "10.0.102.0/24"
-  ]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Project   = "aws-eks-github-actions"
-    Terraform = "true"
-  }
-}
 
 resource "aws_ecr_repository" "app" {
   name                 = var.ecr_repository_name
@@ -68,6 +70,12 @@ resource "aws_ecr_repository" "app" {
   image_scanning_configuration {
     scan_on_push = true
   }
+
+  tags = {
+    Project   = "aws-eks-github-actions"
+    Terraform = "true"
+  }
+}
 
   tags = {
     Project   = "aws-eks-github-actions"
